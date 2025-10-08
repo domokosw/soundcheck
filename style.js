88e0f7f3702b2264fbb36f8cbef6aefc8409ce87
domokosw <domokos.wootsch@gmail.com>
2025-10-08 11:55:31 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,4 @@
+document.getElementById("helloBtn")?.addEventListener("click", () => {
+  alert("You clicked the button! 🎉");
+});
+document.getElementById("year").textContent = new Date().getFullYear();
